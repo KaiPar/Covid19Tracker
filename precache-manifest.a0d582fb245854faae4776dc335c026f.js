d2a0d0d5be9deceb779a24497552fbf15ee97566
Kairav Parekh <9643321zapdos#gmail.com>
2021-05-03 23:53:43 +0530

    Updates

---
+++ precache-manifest.a0d582fb245854faae4776dc335c026f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2a1436ebe65b0f7628fe82cf53ed2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1327f173f4d723b8c1d3",
+    "url": "/static/css/2.c1fb602b.chunk.css"
+  },
+  {
+    "revision": "a86184117365d27c10df",
+    "url": "/static/css/main.e8593139.chunk.css"
+  },
+  {
+    "revision": "1327f173f4d723b8c1d3",
+    "url": "/static/js/2.0b1323f6.chunk.js"
+  },
+  {
+    "revision": "3364d0d8e6179deab9acd4ed9c02496a",
+    "url": "/static/js/2.0b1323f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a86184117365d27c10df",
+    "url": "/static/js/main.f4f8a5ff.chunk.js"
+  },
+  {
+    "revision": "4265dbd31fdb69dfc3ca",
+    "url": "/static/js/runtime-main.82279cab.js"
+  },
+  {
+    "revision": "e586ae794e267aaabe6c233ccb73a591",
+    "url": "/static/media/coding1.e586ae79.png"
+  },
+  {
+    "revision": "21624d040fc8ff86b96407734d90cee7",
+    "url": "/static/media/coding2.21624d04.png"
+  }
+]);
